e44d2ce9ecabca4bdc3813050266164256deb6ff
lifeifan <1228585997@qq.com>
2020-11-23 22:26:19 +0800

    图表修改

--- src/views/modules/sys/components/chartView/chartTopright.vue
+++ src/views/modules/sys/components/chartView/chartTopright.vue
@@ -11,8 +11,8 @@ export default {
 	data() {
 		return {
 			option: {},
-			xData:['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
-			yData:[11, 11, 15, 13, 12, 13, 10]
+			xData: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+			yData: [18, 58, 15, 45, 57, 23, 25]
 		};
 	},
 	mounted() {
@@ -21,11 +21,6 @@ export default {
 	methods: {
 		getEchartsData() {
 			this.option = {
-				 title : {
-				        text: '体脂',
-				        x:'center'
-				    },
-				//悬停
 				tooltip: {
 					trigger: 'axis'
 				},
@@ -34,33 +29,51 @@ export default {
 					{
 						type: 'category',
 						boundaryGap: false,
-						data: this.xData,
-						axisLine: {
-							show: false
-						},
-						axisTick:{
-							show: false
-							},
-						
-					},
-					
+						data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+					}
 				],
 				yAxis: [
 					{
 						type: 'value',
-						axisLine: {
-							show: false
-						},
-						axisTick:{
-							show: false
-							},
+						splitLine: {
+							// 控制Y轴的分隔线(辅助线)
+							show: false, // 默认显示，属性show控制显示与否
+							lineStyle: {
+								// 属性lineStyle（详见lineStyle）控制线条样式
+								color: ['#ccc'],
+								width: 1,
+								type: 'solid'
+							}
+						}
 					}
 				],
 				series: [
 					{
-						name: '体脂',
+						name: '最高气温',
 						type: 'line',
-						data: this.yData
+						data: [11, 11, 15, 13, 12, 13, 10],
+						
+					},
+					{
+						name: '测试',
+						type: 'bar',
+						barWidth :40,//柱图宽度
+						itemStyle: {
+							normal: {
+								color: '#ab78ba', //柱状颜色
+								
+								label: {
+									show: false, //柱头数字
+									position: 'top',
+									textStyle: {
+										fontSize: '20',
+										fontFamily: '微软雅黑',
+										fontWeight: 'bold'
+									}
+								}
+							}
+						},
+						data: [1, 100, 28, 50, 30, 20,78]
 					}
 				]
 			};
