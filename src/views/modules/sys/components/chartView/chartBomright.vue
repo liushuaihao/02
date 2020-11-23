bf7b2a4845583478e9fa192a12e0a586ee01f39e
lifeifan <1228585997@qq.com>
2020-11-23 21:54:36 +0800

    图表显示完成

---
+++ src/views/modules/sys/components/chartView/chartBomright.vue
@@ -0,0 +1,76 @@
+<template>
+	<div class="chartBomright"><chartTemp :option="option"></chartTemp></div>
+</template>
+
+<script>
+import chartTemp from '@/components/chartTemp';
+export default {
+	components: {
+		chartTemp
+	},
+	data() {
+		return {
+			option: {},
+			xData:['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+			yData:[11, 11, 15, 13, 12, 13, 10]
+		};
+	},
+	mounted() {
+		this.getEchartsData();
+	},
+	methods: {
+		getEchartsData() {
+			this.option = {
+				 title : {
+				        text: '血红蛋白',
+				        x:'center'
+				    },
+				//悬停
+				tooltip: {
+					trigger: 'axis'
+				},
+				calculable: true,
+				xAxis: [
+					{
+						type: 'category',
+						boundaryGap: false,
+						data: this.xData,
+						axisLine: {
+							show: false
+						},
+						axisTick:{
+							show: false
+							},
+						
+					},
+					
+				],
+				yAxis: [
+					{
+						type: 'value',
+						axisLine: {
+							show: false
+						},
+						axisTick:{
+							show: false
+							},
+					}
+				],
+				series: [
+					{
+						name: '血红蛋白',
+						type: 'line',
+						data: this.yData
+					}
+				]
+			};
+		}
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.chartBomright {
+	height: 300px;
+}
+</style>
---
+++ src/views/modules/sys/components/chartView/chartTopright.vue
@@ -0,0 +1,76 @@
+<template>
+	<div class="chartTopright"><chartTemp :option="option"></chartTemp></div>
+</template>
+
+<script>
+import chartTemp from '@/components/chartTemp';
+export default {
+	components: {
+		chartTemp
+	},
+	data() {
+		return {
+			option: {},
+			xData:['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+			yData:[11, 11, 15, 13, 12, 13, 10]
+		};
+	},
+	mounted() {
+		this.getEchartsData();
+	},
+	methods: {
+		getEchartsData() {
+			this.option = {
+				 title : {
+				        text: '体脂',
+				        x:'center'
+				    },
+				//悬停
+				tooltip: {
+					trigger: 'axis'
+				},
+				calculable: true,
+				xAxis: [
+					{
+						type: 'category',
+						boundaryGap: false,
+						data: this.xData,
+						axisLine: {
+							show: false
+						},
+						axisTick:{
+							show: false
+							},
+						
+					},
+					
+				],
+				yAxis: [
+					{
+						type: 'value',
+						axisLine: {
+							show: false
+						},
+						axisTick:{
+							show: false
+							},
+					}
+				],
+				series: [
+					{
+						name: '体脂',
+						type: 'line',
+						data: this.yData
+					}
+				]
+			};
+		}
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.chartTopright {
+	height: 300px;
+}
+</style>
