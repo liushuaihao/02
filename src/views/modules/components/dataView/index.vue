f3b761b63a0ac56e079cdd7f81d0c754076c2973
tb659 <782931122@qq.com>
2020-12-02 22:35:54 +0800

    竞技状态分析：原始数据/统计对比

--- src/views/modules/components/dataView/index.vue
+++ src/views/modules/components/dataView/index.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
     <el-form class="el-form--inline" :model="formQurey" size="small">
-      <el-form-item label="指标选择:">
+      <el-form-item style="margin-right:50px">
+        <el-radio v-model="comparison" :label="1">同指标-运动员对比</el-radio>
+        <el-radio v-model="comparison" :label="2">同运动员-指标统计</el-radio>
+      </el-form-item>
+      <el-form-item v-if="comparison === 1" label="指标选择:">
         <el-select v-model="value" placeholder="请选择">
-          <el-option :label="item.label" :value="item.value" :key="item.value" v-for="(item, index) in option"></el-option>
+          <el-option :label="item.label" :value="item.value" :key="item.value" v-for="item in targetOption"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item v-else label="运动员选择:">
+        <el-select v-model="value" placeholder="请选择">
+          <el-option :label="item.label" :value="item.value" :key="item.value" v-for="item in sporterOption"></el-option>
         </el-select>
       </el-form-item>
     </el-form>
-    <el-table :data="tableData" style="width: 100%" align="center">
+    <el-table :data="comparison === 1 ? targetData : sporterData" style="width: 100%" align="center">
       <el-table-column prop="name" label="姓名" min-width="100"> </el-table-column>
       <el-table-column prop="mean" label="均值" min-width="100"> </el-table-column>
       <el-table-column prop="max" label="最大" min-width="100"> </el-table-column>
@@ -24,61 +33,33 @@ export default {
     return {
       formQurey: {},
       value: 1,
-      option: [
-        {
-          label: '体重',
-          value: 0
-        },
-        {
-          label: '体脂',
-          value: 1
-        },
-        {
-          label: '血红蛋白',
-          value: 2
-        },
-        {
-          label: '血压',
-          value: 3
-        }
+      comparison: 1, // 对比参照 1指标 2运动员
+      targetOption: [
+        { label: '体重', value: 0 },
+        { label: '体脂', value: 1 },
+        { label: '血红蛋白', value: 2 },
+        { label: '血压', value: 3 }
       ],
-      tableData: [
-        {
-          id: 0,
-          mean: '1',
-          max: '2',
-          min: '',
-          std: '1',
-          median: '2',
-          name: '王小虎'
-        },
-        {
-          id: 1,
-          mean: '1',
-          max: '2',
-          min: '',
-          std: '1',
-          median: '2',
-          name: '王小虎'
-        },
-        {
-          id: 2,
-          mean: '1',
-          max: '2',
-          min: '',
-          std: '1',
-          median: '2',
-          name: '王小虎'
-        },
-        {
-          id: 3,
-          mean: '1',
-          max: '2',
-          min: '',
-          std: '1',
-          median: '2',
-          name: '王小虎'
-        }
+      sporterOption: [
+        { label: '体重', value: 0 },
+        { label: '体脂', value: 1 },
+        { label: '血红蛋白', value: 2 },
+        { label: '血压', value: 3 }
+      ],
+      targetData: [
+        { id: 0, mean: '1', max: '2', min: '1', std: '1', median: '2', name: '赵小芳' },
+        { id: 1, mean: '1', max: '2', min: '1', std: '1', median: '2', name: '孙宏宇' },
+        { id: 2, mean: '1', max: '2', min: '1', std: '1', median: '2', name: '张东凡' },
+        { id: 3, mean: '1', max: '2', min: '1', std: '1', median: '2', name: '李思儒' }
+      ],
+      sporterData: [
+        { id: 0, mean: '1', max: '2', min: '1', std: '1', median: '2', name: 'BMI' },
+        { id: 1, mean: '1', max: '2', min: '1', std: '1', median: '2', name: '体脂率' },
+        { id: 2, mean: '1', max: '2', min: '1', std: '1', median: '2', name: '体重' },
+        { id: 4, mean: '1', max: '2', min: '1', std: '1', median: '2', name: '血红蛋白' },
+        { id: 5, mean: '1', max: '2', min: '1', std: '1', median: '2', name: '血尿素' },
+        { id: 6, mean: '1', max: '2', min: '1', std: '1', median: '2', name: '血睾酮' },
+        { id: 7, mean: '1', max: '2', min: '1', std: '1', median: '2', name: '皮质醇' }
       ]
     }
   }
--- src/views/modules/components/originalView/index.vue
+++ src/views/modules/components/originalView/index.vue
@@ -1,15 +1,29 @@
 <template>
   <div>
     <el-table :data="tableData" style="width: 100%" align="center">
-      <el-table-column prop="median" label="训练时间" min-width="100"> </el-table-column>
-      <el-table-column prop="mean" label="性别" min-width="100"> </el-table-column>
-      <el-table-column prop="id" label="年龄" min-width="100"> </el-table-column>
-      <el-table-column prop="median" label="身高" min-width="100"> </el-table-column>
-      <el-table-column prop="median" label="体重" min-width="100"> </el-table-column>
+      <el-table-column fixed prop="name" label="姓名" min-width="100"> </el-table-column>
+      <el-table-column label="性别" min-width="100">
+        <template slot-scope="scope">
+          {{ scope.row.sex === 1 ? '男' : '女' }}
+        </template>
+      </el-table-column>
+      <el-table-column prop="age" label="年龄" min-width="100"> </el-table-column>
+      <el-table-column prop="height" label="身高" min-width="100">
+        <template slot-scope="scope">
+          {{ scope.row.height + 'cm' }}
+        </template>
+      </el-table-column>
+      <el-table-column prop="time" label="时间" min-width="100"> </el-table-column>
+      <el-table-column prop="median" label="体重" min-width="100">
+        <template slot-scope="scope">
+          {{ scope.row.weight + 'kg' }}
+        </template>
+      </el-table-column>
+      <el-table-column prop="bmi" label="BMI" min-width="100"> </el-table-column>
       <el-table-column prop="median" label="血红蛋白" min-width="100"> </el-table-column>
       <el-table-column prop="median" label="血尿素" min-width="100"> </el-table-column>
-      <el-table-column prop="median" label="血乳酸" min-width="100"> </el-table-column>
-      <el-table-column prop="median" label="血压" min-width="100"> </el-table-column>
+      <el-table-column prop="median" label="血睾酮" min-width="100"> </el-table-column>
+      <el-table-column prop="median" label="皮质醇" min-width="100"> </el-table-column>
     </el-table>
   </div>
 </template>
@@ -20,60 +34,16 @@ export default {
     return {
       value: 1,
       option: [
-        {
-          label: '体重',
-          value: 0
-        },
-        {
-          label: '体脂',
-          value: 1
-        },
-        {
-          label: '血红蛋白',
-          value: 2
-        },
-        {
-          label: '血压',
-          value: 3
-        }
+        { label: '体重', value: 0 },
+        { label: '体脂', value: 1 },
+        { label: '血红蛋白', value: 2 },
+        { label: '血压', value: 3 }
       ],
       tableData: [
-        {
-          id: 0,
-          mean: '1',
-          max: '2',
-          min: '',
-          std: '1',
-          median: '2',
-          name: '王小虎'
-        },
-        {
-          id: 1,
-          mean: '1',
-          max: '2',
-          min: '',
-          std: '1',
-          median: '2',
-          name: '王小虎'
-        },
-        {
-          id: 2,
-          mean: '1',
-          max: '2',
-          min: '',
-          std: '1',
-          median: '2',
-          name: '王小虎'
-        },
-        {
-          id: 3,
-          mean: '1',
-          max: '2',
-          min: '',
-          std: '1',
-          median: '2',
-          name: '王小虎'
-        }
+        { id: 0, sex: 1, age: 20, height: '176', weight: 56, time: '2020-02-02', bmi: '20', fat: '1', median: '2', name: '王小虎' },
+        { id: 1, sex: 1, age: 20, height: '166', weight: 58, time: '2020-02-02', bmi: '34', fat: '1', median: '2', name: '王小虎' },
+        { id: 2, sex: 1, age: 20, height: '178', weight: 65, time: '2020-02-02', bmi: '25', fat: '1', median: '2', name: '王小虎' },
+        { id: 3, sex: 1, age: 20, height: '169', weight: 53, time: '2020-02-02', bmi: '22', fat: '1', median: '2', name: '王小虎' }
       ]
     }
   }
