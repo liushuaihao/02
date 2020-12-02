bcd28b329abac4346fcb419bb4ea6164629b100f
liushuaihao <719586323@qq.com>
2020-12-02 09:15:15 +0800

    q

--- src/api/athlete/index.js
+++ src/api/athlete/index.js
@@ -1,16 +1,20 @@
-import http from '@/utils/request'
+import axios from "axios";
 
-const url = '/request-athlete-basicinfo'
+export function getHelpRouter(router) {
+  let host = 'http://101.200.153.53:31546/';
+  return new Promise(resolve => {
+    axios({
+      baseURL: host,
+      url: 'request-athlete-basicinfo',
+      method: "post",
+      headers: { "Content-Type": "text/plain; charset=utf-8" },
+      data: {}
+    })
+      .then(res => {
+        resolve(content);
+      })
+      .catch(err => {
 
-/**
- * @returns
- */
-export function basicinfo(data) {
-  return http({
-    url,
-    method: 'post',
-    data: {
-      ...data
-    }
-  })
+      });
+  });
 }
